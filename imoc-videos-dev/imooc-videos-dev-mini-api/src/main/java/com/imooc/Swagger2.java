7862d3481f071aca40b467afb693bac62cc53e83
Zehua Wang <transforminottawa@gmail.com>
2019-04-24 20:00:55 -0400

    Create a configuration class for Swagger2

---
+++ imoc-videos-dev/imooc-videos-dev-mini-api/src/main/java/com/imooc/Swagger2.java
@@ -0,0 +1,15 @@
+package com.imooc;
+
+import org.springframework.context.annotation.Configuration;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+/**
+ * Swagger2 配置类
+ */
+@Configuration
+@EnableSwagger2
+public class Swagger2 {
+
+
+
+}
