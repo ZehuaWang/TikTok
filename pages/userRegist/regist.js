ea1816e7df835ab16a467c3297c1f6dbddb178a2
Zehua Wang <transforminottawa@gmail.com>
2019-04-24 21:39:01 -0400

    Set the app.js and regist.js

--- pages/userRegist/regist.js
+++ pages/userRegist/regist.js
@@ -3,5 +3,20 @@ const app = getApp()
 Page({
     data: {
 
+    },
+
+    doRegist: function(e) {
+      var formObject = e.detail.value;
+      var username = formObject.username;
+      var password = formObject.password;
+
+      //简单验证非空
+      if(username.length == 0 || password.length == 0) {
+        wx.showToast({
+          title: '用户名密码不能为空',
+          icon:'none',
+          duration: 3000
+        })
+      }
     }
 })
