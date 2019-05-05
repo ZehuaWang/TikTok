a9e38c3d9d199ef5a22b745cad0c7787c28915e0
Zehua Wang <transforminottawa@gmail.com>
2019-05-04 20:51:50 -0400

    Add the log out method in js.

--- pages/mine/mine.js
+++ pages/mine/mine.js
@@ -9,6 +9,24 @@ Page({
 
   onLoad: function (params) {
 
-  }
+  },
 
+  logout: function () {
+
+    var user = app.userInfo;
+
+    wx.request({
+      url:serverUrl + '/logout?userId=' + user.id,
+      method: "POST",
+      header: {
+        'content-type' : 'application/json'
+      },
+      success: function (res) {
+        console.log(res.data);
+        wx.hideLoading();
+        if(res.data.status == 200) {}
+      }
+    })
+    
+  }
 })
