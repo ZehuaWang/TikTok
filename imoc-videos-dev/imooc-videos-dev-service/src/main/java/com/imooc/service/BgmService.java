c5f164b5eb82d9f5f5f9ac9dbb371b13a3011828
Zehua Wang <transforminottawa@gmail.com>
2019-05-13 18:47:58 -0400

    Add the interface for query the bgm list

---
+++ imoc-videos-dev/imooc-videos-dev-service/src/main/java/com/imooc/service/BgmService.java
@@ -0,0 +1,16 @@
+package com.imooc.service;
+
+import com.imooc.pojo.Bgm;
+
+import java.util.List;
+
+public interface BgmService {
+
+    /**
+     * query the bgm list
+     * @return
+     */
+    public List<Bgm> queryBgmList();
+
+
+}
