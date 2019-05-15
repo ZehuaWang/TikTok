a0cafa1b9c6875e1d65ddd5b516131ddbd03292f
Zehua Wang <transforminottawa@gmail.com>
2019-05-14 21:16:53 -0400

    Finish the video upload business logic.

--- imoc-videos-dev/imooc-videos-dev-mini-api/src/main/java/com/imooc/controller/VideoController.java
+++ imoc-videos-dev/imooc-videos-dev-mini-api/src/main/java/com/imooc/controller/VideoController.java
@@ -1,14 +1,85 @@
 package com.imooc.controller;
 
+import com.imooc.utils.IMoocJSONResult;
 import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiImplicitParam;
+import io.swagger.annotations.ApiImplicitParams;
+import io.swagger.annotations.ApiOperation;
+import org.springframework.util.StringUtils;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.InputStream;
 
 @RestController
 @Api(value = "Video Related Business Logic Interface", tags = {"Video Related Business Logic Controller"})
 @RequestMapping("/video")
 public class VideoController {
 
-    
+    @ApiOperation(value = "upload video", notes = "interface for video upload")
+    @ApiImplicitParams({
+            @ApiImplicitParam(name = "userId", required = true),
+            @ApiImplicitParam(name = "bgmId", required = true),
+            @ApiImplicitParam(name = "videoSeconds", required = true),
+            @ApiImplicitParam(name = "videoWidth", required = true),
+            @ApiImplicitParam(name = "videoHeight", required = true),
+            @ApiImplicitParam(name = "description", required = false)
+    })
+    @PostMapping("/upload")
+    public IMoocJSONResult upload(String userId, String bgmId, double videoSeconds, int videoWidth,
+                                  int videoHeight, String desc, @RequestParam("file")MultipartFile files) throws Exception {
 
+        if(StringUtils.isEmpty(userId)) {return IMoocJSONResult.errorMsg("User Id can not be empty");}
+
+        //文件保存的命名空间
+        String fileSpace = "/Users/apple/Desktop/scala/TikTok/imoc-user-file";
+
+        //保存到数据库中的相对路径
+        String uploadPathDB = "/" + userId + "/video";
+
+        FileOutputStream fileOutputStream = null;
+        InputStream inputStream = null;
+
+        try{
+
+            if(files != null) {
+                String fileName = files.getOriginalFilename();
+                if(!StringUtils.isEmpty(fileName)) {
+                    //文件上传的最终保存的路径
+                    String finalVideoPath = fileSpace+uploadPathDB+"/"+fileName;
+
+                    //设置数据库的保存的路径
+                    uploadPathDB += ("/" + fileName);
+
+                    File outFile = new File(finalVideoPath);
+
+                    if(outFile.getParentFile() != null || !outFile.getParentFile().isDirectory()) {
+                        outFile.getParentFile().mkdirs();
+                    }
+
+                    fileOutputStream = new FileOutputStream(outFile);
+                    inputStream = files.getInputStream();
+                    org.apache.commons.io.IOUtils.copy(inputStream,fileOutputStream);
+                }
+
+            } else {
+                return IMoocJSONResult.errorMsg("Error when upload...");
+            }
+
+        } catch (Exception e) {
+            e.printStackTrace();
+            return IMoocJSONResult.errorMsg("Error when upload");
+        } finally {
+            if(fileOutputStream != null) {
+                fileOutputStream.flush();
+                fileOutputStream.close();
+            }
+        }
+        return IMoocJSONResult.ok();
+    }
 }
