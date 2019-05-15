96958657a70cdd8b96b01b889ff822b7471b866a
Zehua Wang <transforminottawa@gmail.com>
2019-05-14 20:50:43 -0400

    Add the related js to request the bgm list from database

--- pages/chooseBgm/chooseBgm.js
+++ pages/chooseBgm/chooseBgm.js
@@ -2,10 +2,34 @@ const app = getApp()
 
 Page({
     data: {
+      bgmList: [],
+      serverUrl: ""
     },
 
     onLoad: function () {
-
+      var me = this;
+      wx.showToast({
+        title: 'Please Wait...'
+      });
+      var serverUrl = app.serverUrl;
+      wx.request({
+        url: serverUrl+'/bgm/list',
+        method: "POST",
+        header:{
+          'content-type' : 'application/json'
+        },
+        success: function(res) {
+          console.log(res.data);
+          wx.hideLoading();
+          if(res.data.status == 200) {
+            var bgmList = res.data.data;
+            me.setData({
+              bgmList: bgmList,
+              serverUrl: serverUrl
+            });
+          }
+        }
+      })
     }
 })
 
