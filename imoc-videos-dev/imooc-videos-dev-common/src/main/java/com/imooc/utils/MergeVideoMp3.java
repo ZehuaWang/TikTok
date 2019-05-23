cdd18aab314023f5cfb1322313dd08d0d66ac4c1
Zehua Wang <transforminottawa@gmail.com>
2019-05-22 20:07:57 -0400

    Initialize the merge video and audio tool class

---
+++ imoc-videos-dev/imooc-videos-dev-common/src/main/java/com/imooc/utils/MergeVideoMp3.java
@@ -0,0 +1,41 @@
+package com.imooc.utils;
+
+import org.apache.tomcat.jni.Proc;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class MergeVideoMp3 {
+
+    private String ffmpeg;
+
+    public MergeVideoMp3(String ffmpeg) {
+        super();
+        this.ffmpeg = ffmpeg;
+    }
+
+    public void convertor(String videoInputPath, String mp3InputPath, double seconds, String videoOutputPath) throws Exception {
+        List<String> command = new ArrayList<>();
+        command.add(ffmpeg);
+        command.add("-i");
+        command.add(mp3InputPath);
+        command.add("-i");
+        command.add(videoInputPath);
+        command.add("-t");
+        command.add(String.valueOf(seconds));
+        command.add("-y");
+        command.add(videoOutputPath);
+
+        command.stream().forEach(c -> System.out.print(c));
+
+        ProcessBuilder builder = new ProcessBuilder(command);
+        Process process = builder.start();
+    }
+
+    public static void main(String[] args) throws Exception {
+        MergeVideoMp3 mergeVideoMp3 = new MergeVideoMp3("/Users/apple/Desktop/scala/ffmpeg/ffmpeg/bin/ffmpeg");
+        mergeVideoMp3.convertor("/Users/apple/Desktop/scala/ffmpeg/ffmpeg/bin/cat.mp4","/Users/apple/Desktop/scala/ffmpeg/ffmpeg/bin/pokerface.mp3",7,"/Users/apple/Desktop/scala/ffmpeg/ffmpeg/bin/catwithbgm.mp4");
+    }
+
+
+}
