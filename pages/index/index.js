c4109754fd30d2a01c7a4824b6ff97b76c5e5fb4
Zehua Wang <transforminottawa@gmail.com>
2019-05-31 21:29:07 -0400

    Add the get all video request at the page

--- pages/index/index.js
+++ pages/index/index.js
@@ -3,6 +3,10 @@ const app = getApp()
 Page({
   data: {
     // 用于分页的属性
+    totalPage: 1,
+    page:1,
+    videoList:[],
+    serverUrl: "",
     screenWidth: 350,
   },
 
@@ -12,5 +16,23 @@ Page({
     me.setData({
       screenWidth: screenWidth,
     });
+
+    //获取当前的分页数
+    var page = me.data.page;
+    var serverUrl = app.serverUrl;
+
+    wx.showLoading({
+      title: 'Please Wait'
+    })
+
+    wx.request({
+      url: serverUrl + '/video/showAll?page=' + page,
+      method: "POST",
+      success: function(res) {
+        wx.hideLoading();
+        console.log(res.data);
+      }
+    })
+
   }
 })
