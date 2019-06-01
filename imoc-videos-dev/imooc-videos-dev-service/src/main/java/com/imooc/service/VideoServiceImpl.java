af63f02ca7e26cbfe925dc1bede86ec3d27c053a
Zehua Wang <transforminottawa@gmail.com>
2019-05-31 20:57:00 -0400

    Add the paganation video query logic in service

--- imoc-videos-dev/imooc-videos-dev-service/src/main/java/com/imooc/service/VideoServiceImpl.java
+++ imoc-videos-dev/imooc-videos-dev-service/src/main/java/com/imooc/service/VideoServiceImpl.java
@@ -1,18 +1,28 @@
 package com.imooc.service;
 
+import com.github.pagehelper.PageHelper;
+import com.github.pagehelper.PageInfo;
 import com.imooc.mapper.VideosMapper;
+import com.imooc.mapper.VideosMapperCustom;
 import com.imooc.pojo.Videos;
+import com.imooc.pojo.vo.VideosVO;
+import com.imooc.utils.PageResult;
 import org.n3r.idworker.Sid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Propagation;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.List;
+
 @Service
 public class VideoServiceImpl implements VideoService{
 
     @Autowired
     private VideosMapper videosMapper;
+
+    @Autowired
+    private VideosMapperCustom videosMapperCustom;
 
     @Autowired
     private Sid sid;
@@ -33,5 +43,20 @@ public class VideoServiceImpl implements VideoService{
         videos.setId(videoId);
         videos.setCoverPath(coverPath);
         videosMapper.updateByPrimaryKeySelective(videos);
+    }
+
+    @Transactional(propagation = Propagation.REQUIRED)
+    @Override
+    public PageResult getAllVideos(Integer page, Integer pageSize) {
+        PageHelper.startPage(page,pageSize);
+        List<VideosVO> list = videosMapperCustom.queryAllVideos();
+        PageInfo<VideosVO> pageList = new PageInfo<>(list);
+        PageResult pageResult = new PageResult();
+        pageResult.setPage(page);
+        pageResult.setTotal(pageList.getPages());
+        pageResult.setRows(list);
+        pageResult.setRecords(pageList.getTotal());
+
+        return pageResult;
     }
 }
